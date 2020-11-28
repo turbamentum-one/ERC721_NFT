bd45641c50e74357597cf2c0dbecc4e22a2a70e2
alex maslakov <alex@serendipia.email>
2020-11-28 14:15:22 +0000

    wip

---
+++ src/contracts/Color.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+import "./ERC721Full.sol";
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Color is ERC721Full {
+
+    string[] public colors;
+    mapping(string => bool) _colorExists;
+
+    constructor() ERC721Full("Color", "COLOR") public {
+    }
+
+    function mint(string memory _color) public {
+        require(!_colorExists[_color]);
+        uint _id = colors.push(_color);
+        _mint(msg.sender, _id);
+        _colorExists[_color] = true;
+    }
+}
